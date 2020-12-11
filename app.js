78b6aba1ce952fb76eb145920c91cb9184c08b29
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-12-11 16:05:05 +0200

    change this in methods

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
 const magicBook = {
-    recipes: {
-      golem: {
-        bone: 10,
-        stone: 100,
-        nail: 100,
-        poo: 53
-      },
-      littleDevil: {
-        lava: 10,
-        evil: 999
-      },
-    }
-  };
-  
-  const kitchen = {
-    bone: 999,
-    stone: 999,
-    nail: 999,
-    poo: 999,
-    lava: 999,
-    evil: 999,
-    kettle: {
-      material: 'metal',
-      ingridients: {
-        addIngridientToKettle(nameOfIngridient, amount) {
-            prop = nameOfIngridient;
-            this[prop] = amount;
-          for (let key in kitchen) {
-            if (key === prop)
-              kitchen[key] = kitchen[key] - amount;
-          }
-        },
-        cook(recipe) {
-          for (let prop in recipe) {
-            if (this[prop] < recipe[prop]) {
-              return console.warn('not enough ingredients');
-            }
-          }
-          return console.log("Lets's cooking!");
-        }
+  recipes: {
+    golem: {
+      bone: 10,
+      stone: 100,
+      nail: 100,
+      poo: 53
+    },
+    littleDevil: {
+      lava: 10,
+      evil: 999
+    },
+  }
+};
+
+const kitchen = {
+  bone: 999,
+  stone: 999,
+  nail: 999,
+  poo: 999,
+  lava: 999,
+  evil: 999,
+  kettle: {
+    material: 'metal',
+    ingridients: {}
+},
+  addIngridientToKettle(nameOfIngridient, amount) {
+    this[nameOfIngridient] = this[nameOfIngridient] - amount;
+    this.kettle.ingridients[nameOfIngridient] =  amount;
+},
+  cook(recipe) { 
+    for (let prop in recipe) {
+      if (this.kettle.ingridients[prop]  < recipe[prop]) {
+        return console.warn('not enough ingredients');
       }
     }
-  };
-  
-  kitchen.kettle.ingridients.addIngridientToKettle("bone", 9);
-  kitchen.kettle.ingridients.addIngridientToKettle("stone", 100);
-  kitchen.kettle.ingridients.addIngridientToKettle("nail", 100);
-  kitchen.kettle.ingridients.addIngridientToKettle("poo", 53);
-  kitchen.kettle.ingridients.addIngridientToKettle("kuskus", 53);
+    return console.log("Lets's cooking!");
+  }
+}
 
-  kitchen.kettle.ingridients.cook(magicBook.recipes.golem);
+kitchen.addIngridientToKettle('bone', 1);
+kitchen.addIngridientToKettle("stone", 100);
+kitchen.addIngridientToKettle("nail", 100);
+kitchen.addIngridientToKettle("poo", 53);
+
+kitchen.cook(magicBook.recipes.golem);
+
+console.log(kitchen); 
